b7282fe0ee6a24eec3b89c2b2e0cd096e27b15f9
Epik2 <savalle.florian@sfr.fr>
2022-03-18 10:10:13 +0100

    Ajoute index.css et début index

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 
 <head>
     <meta charset="utf-8">
-    <title>VMS | Login</title>
+    <title>VMS | Homepage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
     <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./stylesheets/login.css">
+    <link rel="stylesheet" href="./stylesheets/index.css">
 </head>
 
 <body>
-
+    <main>
+        <h1>Welcome on VMS</h1>
+    </main>
 </body>
 
 </html>
