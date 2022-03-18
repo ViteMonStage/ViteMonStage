fe41a9d5cd7179e07efbeedee5746336143ec354
Tom ANTOINE <tom.antoine@viacesi.fr>
2022-03-18 13:18:50 +0100

    User profile html et css + ajout icon et img

--- profile_user.php
+++ profile_user.php
@@ -5,10 +5,39 @@
     <meta charset="utf-8">
     <title>Titre de la page</title>
     <link rel="stylesheet" href="stylesheets/profile_user.css">
+    <link rel="stylesheet" href="stylesheets/global.css">
     <script src="script.js"></script>
 </head>
 <body>
-    <img src="#" alt="Profile Picture">
-    
+    <div class="profile">
+    <img id="avatar" src="/assets/pictures/default_avatar.png" alt="Profile Picture" height=>
+    <p class="medium">SURNAME Name</p>
+    <p class="small">Campus : Rouen</p>
+    <p class="small">Sector : IT</p>
+    <p class="small">name.surname@viacesi.fr</p>
+    <p class="small">Male, 20 Years Old</p>
+    <img id="editbutton" src="assets\icons\edit-regular.svg" onclick="openImg()" height="24px"> 
+    </div>
+
+    <div class="wishlist">
+    <p class="medium">Wishlist</p>
+        <div class="offerexample">
+            <a href="" class="medium">Offer example</a>
+            <a href="" class="small">Company</a>
+            <p class="mini">Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Ex maxime, ipsam maiores itaque sint ab, corporis est, 
+                commodi quaerat dignissimos laboriosam eaque perspiciatis architecto a nostrum esse autem ut optio!
+            </p>
+            <ul class="list mini">
+                <li id="city">City</li>
+                <li id="dot"><img src="..\assets\icons\circle-solid.svg" height="6px"></li>
+                <li id="publishDate">Publish Date</li>
+                <li id="dot"><img src="..\assets\icons\circle-solid.svg" height="6px"></li>
+                <li id="sector">Sector</li>
+            </ul>
+
+        </div>
+
+    </div>
 </body>
 </html>
