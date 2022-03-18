35883b9eaba53ed80f11be247f7d3dc583a1f02e
Tom ANTOINE <tom.antoine@viacesi.fr>
2022-03-18 10:49:26 +0100

    Ajout page utilisateur

---
+++ profile_user.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="stylesheets/profile_user.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <img src="#" alt="Profile Picture"
+</body>
+</html>
