59c3bcd6114553f7d9f8a2afbb9e26a986c2ca14
Good660 <good660.minecraft@gmail.com>
2022-03-18 09:41:40 +0100

    Ajout des fichiers

---
+++ login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>VMS | Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./stylesheets/login.css">
+</head>
+
+<body>
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="mainbox">
+            <h1>SIGN IN</h1>
+            <label for="mailtbx" class="tbxindicator">Email</label>
+            <input type="email" class="form-control tbx" id="mailtbx" placeholder="mail@example.com">
+            <label for="mailtbx" class="tbxindicator">Password</label>
+            <input type="password" class="form-control tbx" id="passtbx">
+            <!--Ci-dessous le bouton login-->
+            <input type="submit" class="btn-primary btn" id="logbtn" value="LOGIN">
+            <br>
+            <h1>Chiottard</h1>
+            <div class="margin">
+            <a href="signup.html" class="linkl">No accout ? Sign up !</a>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
