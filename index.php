82b6cf6c222eab5f4ec318d42992f56c6025b766
Epik2 <savalle.florian@sfr.fr>
2022-03-18 11:51:15 +0100

    Base de la page index, ajout des images...

--- index.php
+++ index.php
@@ -8,19 +8,45 @@
     <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
     <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./stylesheets/global.css">
     <link rel="stylesheet" href="./stylesheets/index.css">
 </head>
 
 <body>
-    <main>
-        <h1>Welcome on VMS</h1>
-        <img src="assets\pictures\meeting.png" width="40%" height="30%" class="img" id="appl"><img src="assets\pictures\meeting.png" width="40%" height="30%" class="img" id="appl">
-        <img src="assets\pictures\meeting.png" width="40%" height="30%" class="img" id="appl"><img src="assets\pictures\meeting.png" width="40%" height="30%" class="img" id="appl">
+    <h1>Welcome on VMS</h1>
+    <div class="row">
+        <div class="container">
+            <img src="assets\pictures\meeting.png" class="img">
+            <div class="text-block">
+                <h4>Your applications</h4>
+                <p>Keep track your applications here.</p>
+            </div>
+        </div>
+        <div class="container">
+            <img src="assets\pictures\shake.png" class="img">
+            <div class="text-block">
+                <h4>Current offers</h4>
+                <p>Check out the popular offers right now.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container">
+            <img src="assets\pictures\Corporations.png" class="img">
+            <div class="text-block">
+                <h4>Companies</h4>
+                <p>Browse the companies here.</p>
+            </div>
+        </div>
+        <div class="container">
+            <img src="assets\pictures\paper.png" class="img">
+            <div class="text-block">
+                <h4>Personnal informations</h4>
+                <p>Modify your personnal informations here.</p>
+            </div>
+        </div>
+    </div>
 
-
-
-
-    </main>
 </body>
 
 </html>
