fa64288ec6b27e59b7b87ddc99304059f012a060
Good660 <good660.minecraft@gmail.com>
2022-03-18 13:19:21 +0100

    Ajout navbar

--- candidatures.php
+++ candidatures.php
@@ -2,32 +2,34 @@
 <html lang="fr">
 
 <head>
-    <meta charset="utf-8">
-    <title>VMS | Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
-    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./stylesheets/candidature.css">
+  <meta charset="utf-8">
+  <title>VMS | Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./stylesheets/candidatures.css">
+  <link rel="stylesheet" href="./stylesheets/global.css">
 </head>
 
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="./index.php">VMS</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home</span></a>
-      <a class="nav-item nav-link" href="#">Offers</a>
-      <a class="nav-item nav-link" href="#">Candidature</a>
-      <a class="nav-item nav-link" href="#">Profile</a>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand maxi logo" href="./index.php">VMS</a>
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav navbar-align-left navbar-margin">
+        <a class="nav-item nav-link current small" href="#">HOME</span></a>
+        <a class="nav-item nav-link small" href="#">OFFERS</a>
+        <a class="nav-item nav-link small" href="#">APPLICATIONS</a>
+        <a class="nav-item nav-link small" href="#">COMPANIES</a>
+        <a class="nav-item nav-link small navbar-highlight" href="#">PROFILE</a>
+      </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </body>
+<script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </html>
