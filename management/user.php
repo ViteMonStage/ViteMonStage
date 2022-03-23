4813555532d43e67600e7b1824c91ba246e440e8
Epik2 <savalle.florian@sfr.fr>
2022-03-23 13:16:17 +0100

    ddd

--- management/user.php
+++ management/user.php
@@ -3,16 +3,20 @@
 
 <head>
     <meta charset="utf-8">
-    <title>VMS | Login</title>
+    <title>VMS | Homepage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
     <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./stylesheets/login.css">
+    <link rel="stylesheet" href="./stylesheets/management/user.scss">
+    <link rel="stylesheet" href="./stylesheets/global.scss">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
 </head>
 
 <body>
-
+    
 </body>
+
 
 </html>
