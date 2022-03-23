be54af2fc4e400a9fb4005ff3d4b91cf0340d43d
Epik2 <savalle.florian@sfr.fr>
2022-03-23 14:07:18 +0100

    chanclette

--- management/offers.php
+++ management/offers.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
     <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./stylesheets/management/offers.scss">
-    <link rel="stylesheet" href="./stylesheets/global.scss">
+    <link rel="stylesheet" href="/assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/management/offers.scss">
+    <link rel="stylesheet" href="/stylesheets/global.scss">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 
 </head>
