f2fa545e4b645f8129f7f5b7171837c0ddd7078c
Good660 <good660.minecraft@gmail.com>
2022-03-28 09:35:49 +0200

    Ajout page forbidden + modification animation login

---
+++ error/403.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>VMS | 403</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="/assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="/assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/error/403.scss">
+    <link rel="stylesheet" href="/stylesheets/global.scss">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="full">
+        <div class="d-flex align-items-center justify-content-center">
+            <h1 class="maxi">VMS</h1>
+        </div>
+        <div class="d-flex align-items-center justify-content-center">
+            <h1 class="small"><span class="error big">403</span> • You are not allowed to access to this page. You can go back to home by clicking <a href="/index.php">here</a>.</h1>
+        </div>
+    </div>
+</body>
+
+</html>
