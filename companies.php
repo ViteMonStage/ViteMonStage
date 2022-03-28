6bff35deb9ee6efcfcc0410718ac57725e8d19ea
Haytiss <pdespont4@gmail.com>
2022-03-28 17:26:55 +0200

    COMPANIE PHP

--- companies.php
+++ companies.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 
 </head>
-    
+
 <body>
     <!-- Nav bar-->
     <header>
@@ -49,44 +49,9 @@
         </div>
     </div>
     <!-- Resut-->
-    <?php try {
 
-$sql = $pdo->prepare('SELECT company_name,company.description,cityname,zipcode,sector_activity from company 
-INNER JOIN  address on company.id_company = address.id_company
-INNER JOIN city on address.id_city = city.id_city
-');
-$sql->execute();
-$row = $sql->fetchAll();
-}
-
-catch (\PDOException $e) {
-    echo $e->getMessage();
-    echo "   ";
-    echo (int)$e->getCode();
-}?>
     <h2 class="results big"> </h2>
-    <?php include_once dirname(__FILE__) . "/php/companie.php"; displayCompanie();?>
-    <div class="result">
-        <img src="./assets/pictures/logo.jpg" alt="Logo 1" class="logoentreprise">
-        <div class="in_desc">
-            <h3 class="medium"><?php   print_r($row[0][0]); ?></h3>
-            <p class="mini"><?php print_r($row[0][1]); ?></p>
-            <h4 class="mini loca"><?php print_r($row[0][2]) ?> (<?php print_r($row[0][3]); ?>) - <?php print_r($row[0][4]); ?></h4>
-        </div>
-        <a role="button" href="./companies_detail.php" class="small btn detail">See </a>
-    </div>
-
-    <div class="result">
-        <img src="./assets/pictures/logo2.jpg" alt="Logo 2" class="logoentreprise">
-        <div class="in_desc">
-            <h3 class="medium"><?php print_r($row[1][0]) ?></h3>
-            <p class="mini"><?php print_r($row[1][1]) ?></p>
-            <h4 class="mini loca"><?php print_r($row[1][2]) ?> (<?php print_r($row[1][3]) ?>) - <?php print_r($row[1][4]) ?> </h4>
-        </div>
-        <a role="button" href="./companies_detail.php" class="small btn detail "> See </a>
-    </div>
-
-
+    <?php include_once dirname(__FILE__) . "/php/companie.php";displayCompanie(); ?>
     <?php
     include "./php/footer.php"
     ?>
--- php/companie.php
+++ php/companie.php
@@ -1,5 +1,6 @@
 <?php
-function displayCompanie(){
+function displayCompanie()
+{
     try {
         include dirname(__FILE__) . "/db.php"; //Used to get global pdo
         $sql = $pdo->prepare('SELECT company_name,company.description,cityname,zipcode,sector_activity from company 
@@ -8,25 +9,19 @@ function displayCompanie(){
         ');
         $sql->execute();
         $row = $sql->fetchAll();
-        print_r($row[0][0]);
         if (isset($row[0]) == 1) {
-            echo '<div class="result">';
+
             foreach ($row as $value) {
+                echo '<div class="result">';
                 echo '<img src="./assets/pictures/logo.jpg" alt="Logo 1" class="logoentreprise">
             <div class="in_desc">
-            <h3 class="medium">' . $value[1] . '</h3>
-            <p class="mini">' . $value[2] . '</p>
-            <h4 class="mini loca">' . $value[3] . '(' . $value[4] . ') - ' . $value[5] . '</h4>
+            <h3 class="medium">' . $value[0] . '</h3>
+            <p class="mini">' . $value[1] . '</p>
+            <h4 class="mini loca">' . $value[2] . '(' . $value[3] . ') - ' . $value[4] . '</h4>
             </div>
             <a role="button" href="./companies_detail.php" class="small btn detail">See </a>
         </div>';
             }
-
-            echo '</div>';
-        } else {
-            echo '<div class="modal-body">
-        <p class="small">No new notification</p>
-    </div>';
         }
     } catch (\PDOException $e) {
         echo $e->getMessage();
@@ -34,5 +29,3 @@ function displayCompanie(){
         echo (int)$e->getCode();
     }
 }
-
-
