5781d3149a700eef185fdf32d90c5ac3c56ccbff
Good660 <good660.minecraft@gmail.com>
2022-03-29 14:30:35 +0200

    Modifications notifs

--- php/notification.php
+++ php/notification.php
@@ -1,33 +1,41 @@
 <?php
-function displayNotification(){
+function displayNotification($id_user)
+{
     try {
         include dirname(__FILE__) . "/db.php"; //Used to get global pdo
         $stm = $pdo->prepare('SELECT * FROM notification WHERE id_user=?');
-        $stm->bindParam(1, $_SESSION['id_user']);
+        $stm->bindParam(1, $id_user);
         $stm->execute();
         $row = $stm->fetchAll();
         if (isset($row[0]) == 1) {
-            echo '<div class="modal-body">';
-            foreach ($row as $value) {
-                echo '<div class="toast full show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <strong class="me-auto"><span class="badge bg-primary"><i class="fa-solid fa-circle-info"></i> Information</span> ' . $value[1] . '</strong>
-                <small>' . $value[3] . '</small>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        ?><div class="modal-body">
+                <?php foreach ($row as $value) : ?>
+                    <div class="toast full show" role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="toast-header">
+                            <strong class="me-auto"><span class="badge bg-primary"><i class="fa-solid fa-circle-info"></i> Information</span> <?php echo $value[1] ?></strong>
+                            <small><?php echo $value[3] ?></small>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body"><?php echo $value[2] ?></div>
+                    </div>
+                <?php endforeach;
+                ?></div><?php
+                    } else { ?>
+            <div class="modal-body">
+                <p class="small">No new notification</p>
             </div>
-            <div class="toast-body">' . $value[2] . '</div>
-        </div>';
+        <?php }
+                } catch (\PDOException $e) {
+                    echo $e->getMessage();
+                    echo "   ";
+                    echo (int)$e->getCode();
+                }
             }
-
-            echo '</div>';
-        } else {
-            echo '<div class="modal-body">
-        <p class="small">No new notification</p>
-    </div>';
-        }
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-        echo "   ";
-        echo (int)$e->getCode();
-    }
+function getNumberNotification($id_user){
+    include dirname(__FILE__) . "/db.php"; //Used to get global pdo
+    $stm = $pdo->prepare('SELECT * FROM notification WHERE id_user=?');
+    $stm->bindParam(1, $id_user);
+    $stm->execute();
+    $row = $stm->fetchAll();
+    return sizeof($row);
 }
