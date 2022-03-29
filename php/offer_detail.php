8dd40679c87c2cfed3b966264804fed1a797ad66
Good660 <good660.minecraft@gmail.com>
2022-03-29 13:27:29 +0200

    Correction offerdetails

--- php/offer_detail.php
+++ php/offer_detail.php
@@ -19,51 +19,52 @@ function displayOfferdetail()
         $sql->execute();
         $row = $sql->fetchAll();
         if (isset($row[0]) == 1) {
-            foreach ($row as $value) :?>
+            foreach ($row as $value) : ?>
                 <div class="off_desc">
-                <img src="./assets/pictures/logo.jpg" alt="Logo" class="logoentreprise">
-            <div class="off_desc_txt">
-            <h2 class="big"> <?php echo ' . $value[0] . ' ?></h2>
-            <h4 class="small">Intership type : <?php echo  $value[9] ?></h4>
-            <h4 class="small"><?php echo $value[1] ?></h4>
-            <h4 class="small"><?php echo  $value[2]  ( $value[3] )?> </h4>
-            <h4 class="small"><?php echo $value[4]  ?></h4>
-            <h4 class="small"> Number of interns :<?php echo  $value[5]  ?></h4>
-            <h4 class="small">Starting date : <?php echo  $value[6]  ?> - End date : <?php echo  $value[7]  ?></h4>
-            </div>
-                <?php if (!isInWishlist($_SESSION["id_user"], $_GET["id_offer"])) : ?>
-                    <div class="wish">
-                <a class="addlink" href="/php/wishlist.php?add= <?php $_GET["id_offer"]  ?>">
-                    <div class="wish_d1">
-                        <i class="fa-solid fa-star"></i>
+                    <img src="./assets/pictures/logo.jpg" alt="Logo" class="logoentreprise">
+                    <div class="off_desc_txt">
+                        <h2 class="big"> <?php echo $value[0] ?></h2>
+                        <h4 class="small">Intership type : <?php echo  $value[9] ?></h4>
+                        <h4 class="small"><?php echo $value[1] ?></h4>
+                        <h4 class="small"><?php echo  $value[2]?> (<?php echo $value[3] ?>)</h4>
+                        <h4 class="small"><?php echo $value[4]  ?></h4>
+                        <h4 class="small"> Number of interns :<?php echo  $value[5]  ?></h4>
+                        <h4 class="small">Starting date : <?php echo  $value[6]  ?> - End date : <?php echo  $value[7]  ?></h4>
                     </div>
-                    <div class="wish_d2">
-                        <h5 class="mini"> Add to wishlist</h5>
-                    </div>
-                </a>
+                    <?php if (!isInWishlist($_SESSION["id_user"], $_GET["id_offer"])) : ?>
+                        <div class="wish">
+                            <a class="addlink" href="/php/wishlist.php?add=<?php echo $_GET["id_offer"]  ?>">
+                                <div class="wish_d1">
+                                    <i class="fa-solid fa-star"></i>
+                                </div>
+                                <div class="wish_d2">
+                                    <h5 class="mini"> Add to wishlist</h5>
+                                </div>
+                            </a>
+                        </div>
+                </div>
+            <?php else : ?>
+                <div class="wish">
+                    <a class="removelink" href="/php/wishlist.php?remove=<?php echo $_GET["id_offer"] ?> ">
+                        <div class="wish_d1">
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <div class="wish_d2">
+                            <h5 class="mini"> Remove from wishlist</h5>
+                        </div>
+                    </a>
                 </div>
                 </div>
-                 <?php  endif; ?>
-                    <div class="wish">
-                <a class="removelink" href="/php/wishlist.php?remove= <?php $_GET["id_offer"] ?> ">
-                <div class="wish_d1">
-                    <i class="fa-solid fa-star"></i>
-                </div>
-                <div class="wish_d2">
-                    <h5 class="mini"> Remove from wishlist</h5>
-                </div>
-                </a>
-                </div>
-                </div>
+            <?php endif;?>       
                 <div class="off_det">
-            <h3 class="medium"> Offer description </h3>
-            <p class="mini"> <?php $value[8]  ?> </p>
-        </div>
+                    <h3 class="medium"> Offer description </h3>
+                    <p class="mini"> <?php echo $value[8]  ?> </p>
+                </div>
+<?php endforeach;
+            }
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            echo "   ";
+            echo (int)$e->getCode();
         }
-     <?php catch  (\PDOException $e) {
-        echo $e->getMessage();
-        echo "   ";
-        echo (int)$e->getCode(); ?>
     }
-
-}
