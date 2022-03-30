f00d5396c3e817360ea4f5b385507c650021018d
Haytiss <pdespont4@gmail.com>
2022-03-30 18:00:10 +0200

    changement

--- php/offer.php
+++ php/offer.php
@@ -3,11 +3,11 @@ function displayOffers()
 {
     try {
         include dirname(__FILE__) . "/db.php"; //Used to get global pdo
-        $sql = $pdo->prepare('SELECT offer_name,company.company_name,offers.description,cityname,zipcode,offer_date,offers.id_offer from offers
+        $sql = $pdo->prepare('SELECT offer_name,company.company_name,offers.description,cityname,zipcode,offer_date,offers.salary,offers.id_offer from offers
         INNER JOIN company on offers.id_company = company.id_company
         INNER JOIN address on company.id_company = address.id_company
         INNER JOIN city on address.id_city = city.id_city
-        INNER JOIN concern on
+        
         ');
         $sql->execute();
         $row = $sql->fetchAll();
@@ -20,6 +20,7 @@ function displayOffers()
                             <h4 class="small off_company"><?php echo  $value[1] ?></h4>
                         </div>
                         <p class="mini"><?php echo  $value[2] ?></p>
+                        <h4 class="mini"><?php echo  $value[6] ?> €/ months</h4>
                         <h4 class="mini"> <?php echo  $value[3] ?> (<?php echo  $value[4] ?>) - <?php echo  $value[5] ?> </h4>
                     </div>
                     <div class="in_logo">
@@ -27,7 +28,7 @@ function displayOffers()
                             <img src="./assets/pictures/logo2.jpg" alt="Logo" class="logoentreprise">
                         </div>
                         <div>
-                            <a href="offers_detail.php?id_offer=<?php echo  $value[6] ?>" role="button" class="small btn see">See Offer</a>
+                            <a href="offers_detail.php?id_offer=<?php echo  $value[7] ?>" role="button" class="small btn see">See Offer</a>
                         </div>
                     </div>
                 </div>
