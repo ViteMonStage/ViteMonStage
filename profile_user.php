d94ec94e8d6094101a990348f144bdadcc994040
Tom ANTOINE <tom.antoine@viacesi.fr>
2022-03-30 16:31:21 +0200

    UPDATE INFOS PROFILE SANS AVATAR

--- profile_user.php
+++ profile_user.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>My Profile Page</title>
     <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
@@ -11,7 +12,7 @@
     <link rel="stylesheet" href="stylesheets/profile_user.scss">
     <link rel="stylesheet" href="stylesheets/global.scss">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-    
+
 </head>
 
 <body>
@@ -23,64 +24,99 @@
         <?php
         include "./php/navbar.php";
         include "./php/profile.php";
+
+
         ?>
     </header>
 
 
 
     <!-- PROFILE BANNER -->
-        <div class="infos">
-            
-            <div class="profile-pic">
-            <form action="php/profile.php" method="put">
-            <img  src=<?php 
-            if(is_file('assets/user_data/avatar/'.$id.'.png' ))
-            {
-                echo'/assets/user_data/avatar/'.$id.'.png';
-            }
-            // elseif('assets/user_data/avatar/'.$id.'.jpg'){
-            //     echo'/assets/user_data/avatar/'.$id.'.jpg'; 
-            // }
-            else{
-                echo"/assets/pictures/default_avatar.png";
-            }
-            ?> id="avatar" alt="Profile Picture">
-                <input id="file" type="file" accept="image/png" onchange="loadFile(event)"/>
+    <div class="infos">
+
+        <div class="profile-pic">
+            <form action="php/profile.php" method="post">
+                <img src=<?php
+                            if (is_file('assets/user_data/avatar/' . $id . '.png')) {
+                                echo '/assets/user_data/avatar/' . $id . '.png';
+                            }
+                            // elseif('assets/user_data/avatar/'.$id.'.jpg'){
+                            //     echo'/assets/user_data/avatar/'.$id.'.jpg'; 
+                            // }
+                            else {
+                                echo "/assets/pictures/default_avatar.png";
+                            }
+                            ?> id="avatar" alt="Profile Picture">
+                <input id="file" type="file" accept="image/png" onchange="loadFile(event)" />
                 <label id="uploadbtn" for="file">
                     <span class="fa-solid fa-camera"></span>
                     <span>Change Image</span>
                 </label>
-            </div>
-            <ul id="listinfo">
-                <li><input class="medium " placeholder="Lastname" readonly="readonly" name="surname" value="<?php print_r($row[0][0]); ?>"></li>
-                <li><input class="medium " placeholder="Name" readonly="readonly" name="name" value="<?php print_r($row[0][1]); ?>"></li>
-                <li><input class="small " placeholder="Gender" readonly="readonly" name="gender" value="<?php print_r($row[0][2]); ?>"></li>
-                <li><input class="small " placeholder="Email" readonly="readonly" name="email" value="<?php print_r($row[0][3]); ?>"></li>
-                <li><input class="mini " placeholder="Age" readonly="readonly"  name ="age" value="<?php print_r($row[0][4]); ?>"></li>
-                <li><div id="calendar" class="md-form md-outline input-with-post-icon datepicker">
-                                <input  placeholder="Select date" type="date" id="birthtbx" class="form-control tbx mini" name="birthday">
-                            </div></li>
-                <li><select id="listboxpromo" class="form-control tbx mini listbox" name="promotion">
-                    <div class="list-content">
-                    <?php
-                            include "../db.php"; //Used to get global pdo
-                            $stm = $pdo->prepare('SELECT id_promotion, promotion_name, promotion_type FROM promotion INNER JOIN promotion_type ON promotion.id_promotion_type = promotion_type.id_promotion_type'); //query to get promotions and their type
-                            $stm->execute();
-                            $row1 = $stm->fetchAll();
-                                echo '<option>None</option>';
-                            foreach ($row1 as $value) {
-                                echo '<option value='."$value[0]".'>' . $value[1] . ' - ' . $value[2] . '</option>';
-                            }
-                            ?>
-                            </div></li>
-                            <li><input   readonly="readonly" name="promotion" value="<?php print_r($row[0][5]); ?>"></li>
-                <li><input class="mini " placeholder="Campus" readonly="readonly" name="campus" value="<?php print_r($row[0][6]); ?>"></li>
-                <li><input class="mini " placeholder="Promotion type" readonly="readonly" name="promotype" value="<?php print_r($row[0][7]); ?>"></li>
-            </ul>
-            <img id="editbutton" src="assets/icons/edit-white.svg" alt="Edit" role="button" onclick="edit()">
-            <input type="submit" role="button" alt="done"  id="end-editing" name="postbutton" value="">
-            </form>
         </div>
+        <ul id="listinfo">
+            <li><input class="medium " placeholder="Lastname" readonly="readonly" name="surname" value="<?php print_r($row[0][0]); ?>"></li>
+            <li><input class="medium " placeholder="Name" readonly="readonly" name="name" value="<?php print_r($row[0][1]); ?>"></li>
+            <li><input class="small " placeholder="Gender" readonly="readonly" name="gender" value="<?php print_r($row[0][2]); ?>"></li>
+            <li><input class="small " placeholder="Email" readonly="readonly" name="email" value="<?php print_r($row[0][3]); ?>"></li>
+            <li><input class="mini " placeholder="Age" readonly="readonly" name="age" value="<?php print_r($row[0][4]); ?>"></li>
+            <li>
+                <div id="calendar" class="md-form md-outline input-with-post-icon datepicker">
+                    <input placeholder="Select date" type="date" id="birthtbx" class="form-control tbx mini" name="birthday">
+                </div>
+            </li>
+            <li><select id="listboxpromo" class="form-control tbx mini listbox" name="promotion">
+                    <div class="list-content">
+                        <?php
+                            if($row[0][9]==NULL){
+                                echo "<option value='NULL'selected> None </option>";
+                                    }
+                                    else{
+                                    echo '<option value=' . "$row[0][9]" . 'selected>' . $row[0][5] . ' - ' . $row[0][7] . '</option>';
+                                    }
+                        //include "../db.php"; //Used to get global pdo
+                        $stm = $pdo->prepare("SELECT id_promotion, promotion_name, promotion_type FROM promotion INNER JOIN promotion_type ON promotion.id_promotion_type = promotion_type.id_promotion_type WHERE promotion_type != '" . $row[0][7] . "' "); //query to get promotions and their type
+
+                        $stm->execute();
+                        $row1 = $stm->fetchAll();
+                        
+
+                        foreach ($row1 as $value) {
+                            echo '<option value=' . "$value[0]" . '>' . $value[1] . ' - ' . $value[2] . '</option>';
+                        }
+                        ?>
+                        
+                    </div>
+            </li>
+            <li><input class="mini" readonly="readonly" name="promo" value="<?php print_r($row[0][5]); ?>"></li>
+            <li><select id="listboxcampus" class="form-control tbx mini listbox" name="campus">
+                    <div class="list-content">
+                        <?php
+                        // include "../db.php"; //Used to get global pdo
+                        $stm1 = $pdo->prepare("SELECT id_campus, campus_name FROM campus WHERE campus_name != '" . $row[0][6] . "'"); //query to get promotions and their type
+                        $stm1->execute();
+                        $row2 = $stm1->fetchAll();
+                        echo '<option value=' . $row[0][10] . '>' . $row[0][6] . '</option>';
+                        foreach ($row2 as $value1) {
+                            echo '<option value=' . "$value1[0]" . '>' . $value1[1] . '</option>';
+                        }
+                        ?>
+                    </div>
+            </li>
+            <li><input class="mini " placeholder="Campus" readonly="readonly" name="camp" value="<?php print_r($row[0][6]); ?>"></li>
+            <li><input class="mini " placeholder="" readonly="readonly" name="promotype" value="<?php print_r($row[0][7]); ?>"></li>
+        </ul>
+        <img id="editbutton" src="assets/icons/edit-white.svg" alt="Edit" role="button" onclick="edit()">
+        <input type="submit" role="button" alt="done" id="end-editing" name="postbutton" value="postbutton">
+        <?php
+        if (isset($_GET["errorinputs"])) {
+            if ($_GET["errorinputs"] == "1") {
+
+                echo "Error : please fill all inputs";
+            }
+        } ?>
+
+        </form>
+    </div>
 
 
 
@@ -233,43 +269,43 @@
     <div class="row g-0">
         <div class="col-lg-12 col-sm-12">
             <div class="taskbox">
-                
+
                 <div class="useracc row">
                     <ul class="row col-lg-12 list-group list-group-horizontal flex">
                         <div class="col-lg-3">
-                        <li> <label class='switch mini'><input type='checkbox' id="c_search"><span></span></label><span class="mini">Search a company</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="c_modify"><span></span></label><span class="mini">Modify a company</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="c_rate"><span></span></label><span class="mini">Evaluate a company</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="c_delete"><span></span></label><span class="mini">Delete a company</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="c_stats"><span></span></label><span class="mini">See company stats</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="c_search"><span></span></label><span class="mini">Search a company</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="c_modify"><span></span></label><span class="mini">Modify a company</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="c_rate"><span></span></label><span class="mini">Evaluate a company</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="c_delete"><span></span></label><span class="mini">Delete a company</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="c_stats"><span></span></label><span class="mini">See company stats</span></li>
                         </div>
                         <div class="col-lg-3">
-                        <li> <label class='switch mini'><input type='checkbox' id="o_search"><span></span></label><span class="mini">Search an offer</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="o_create"><span></span></label><span class="mini">Create an offer</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="o_modify"><span></span></label><span class="mini">Modify an offer</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="o_delete"><span></span></label><span class="mini">Delete an offer</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="o_stats"><span></span></label><span class="mini">See offer stats</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="o_search"><span></span></label><span class="mini">Search an offer</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="o_create"><span></span></label><span class="mini">Create an offer</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="o_modify"><span></span></label><span class="mini">Modify an offer</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="o_delete"><span></span></label><span class="mini">Delete an offer</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="o_stats"><span></span></label><span class="mini">See offer stats</span></li>
                         </div>
                         <div class="col-lg-3">
-                        <li> <label class='switch mini'><input type='checkbox' id="p_search"><span></span></label><span class="mini">Search a pilot</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="p_create"><span></span></label><span class="mini">Create a pilot</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="p_modify"><span></span></label><span class="mini">Modify pilot</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="p_delete"><span></span></label><span class="mini">Delete pilot</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="p_search"><span></span></label><span class="mini">Search a pilot</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="p_create"><span></span></label><span class="mini">Create a pilot</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="p_modify"><span></span></label><span class="mini">Modify pilot</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="p_delete"><span></span></label><span class="mini">Delete pilot</span></li>
                         </div>
                         <div class="col-lg-3">
-                        <li> <label class='switch mini'><input type='checkbox' id="d_search"><span></span></label><span class="mini">Search delegate</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="d_create"><span></span></label><span class="mini">Create a delegate</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="d_modify"><span></span></label><span class="mini">Modify delegate</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="d_delete"><span></span></label><span class="mini">Delete delegate</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="d_search"><span></span></label><span class="mini">Search delegate</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="d_create"><span></span></label><span class="mini">Create a delegate</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="d_modify"><span></span></label><span class="mini">Modify delegate</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="d_delete"><span></span></label><span class="mini">Delete delegate</span></li>
                         </div>
                         <div class="col-lg-3">
-                        <li> <label class='switch mini'><input type='checkbox' id="st_search"><span></span></label><span class="mini">Search student</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="st_create"><span></span></label><span class="mini">Create a student</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="st_modify"><span></span></label><span class="mini">Modify a student</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="st_delete"><span></span></label><span class="mini">Delete a student</span></li>
-                        <li> <label class='switch mini'><input type='checkbox' id="st_stats"><span></span></label><span class="mini">See students stats</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="st_search"><span></span></label><span class="mini">Search student</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="st_create"><span></span></label><span class="mini">Create a student</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="st_modify"><span></span></label><span class="mini">Modify a student</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="st_delete"><span></span></label><span class="mini">Delete a student</span></li>
+                            <li> <label class='switch mini'><input type='checkbox' id="st_stats"><span></span></label><span class="mini">See students stats</span></li>
                         </div>
-                            
+
                     </ul>
                 </div>
             </div>
@@ -277,7 +313,7 @@
 
 
     </div>
-    
+
     <!-- FOOTER -->
 
 
@@ -289,7 +325,7 @@
 
 
 
-<script src="./js/profile_user.js"></script>
+    <script src="./js/profile_user.js"></script>
 </body>
 
 </html>
