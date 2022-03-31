87be14864f7cb5ea91434b0abe6d416c851c4a4d
Haytiss <pdespont4@gmail.com>
2022-03-31 23:17:00 +0200

    AHI

--- companies_detail.php
+++ companies_detail.php
@@ -27,11 +27,9 @@
     <?php
     ob_start();
     displayCompaniedetails();?>
-    <div class="company_btn">
-        <input type="button" class="small btn company" value="Modify">
-        <input type="button" class="small btn company" value="Modify">
-        <input type="button" class="small btn company" value="Delete">
-    </div>
+    <form class="company_btn" action="./php/visible.php" method="POST"> 
+        <input type="button" class="small btn company" value="Invisible" name="invisible">
+    </form>
     <?php include_once dirname(__FILE__) . "/php/companies.php";
     displayRatingOptions($_SESSION["id_user"],$_GET["id_company"]); 
     displayAllRating($_GET["id_company"]);
