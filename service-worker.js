6dc012cd85504a3e70d2d55b7b1b35cc2081b619
Epik2 <savalle.florian@sfr.fr>
2022-06-13 18:23:32 +0200

    fuck fabien

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,18 @@ var appShellFiles = [
   '/js13kPWA-v1/index.html',
 ];
 
+var gamesImages = [];
+for(var i=0; i<games.length; i++) {
+  gamesImages.push('data/img/'+games[i].slug+'.jpg');
+}
+
+var contentToCache = appShellFiles.concat(gamesImages);
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installation');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Mise en cache globale: app shell et contenu');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
