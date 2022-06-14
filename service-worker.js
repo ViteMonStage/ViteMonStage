e77f691acdb6d2d11a4a1cdb0bfab99bb5970136
Negi <tom.a.antoine@free.fr>
2022-06-14 18:50:38 +0200

    Ajout service worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,28 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installation');
+var cacheName = 'vt:static';
+self.addEventListener('install', function(e) {
+  // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+  e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+          return cache.addAll([
+              './'
+  
+          ]).then(function() {
+              self.skipWaiting();
+          });
+      })
+  );
 });
-
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
-  '/js13kPWA-v1/PWA/LOGO2.png',
-  '/js13kPWA-v1/index.html',
-];
-
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+  // … either respond with the cached object or go ahead and fetch the actual URL
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          if (response) {
+              // retrieve from cache
+              return response;
+          }
+          // fetch as normal
+          return fetch(event.request);
+      })
+  );
+});
