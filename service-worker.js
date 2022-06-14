5c77833345d0d668b22f42af6975b9970f729b51
Epik2 <savalle.florian@sfr.fr>
2022-06-14 17:38:52 +0200

    nique fabien 2

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,24 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installation');
-});
-
-var cacheName = 'js13kPWA-v1';
+var cacheName = 'testcache';
 var appShellFiles = [
-  '/js13kPWA-v1/PWA/LOGO2.png',
-  '/js13kPWA-v1/index.html',
+  '/'
 ];
 
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
-}
 
-var contentToCache = appShellFiles.concat(gamesImages);
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installation');
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Mise en cache globale: app shell et contenu');
-      return cache.addAll(contentToCache);
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
