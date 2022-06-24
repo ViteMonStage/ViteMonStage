79729913cfe2d1d0c3e553f823c7f36cf3eb7e53
Good660 <good660.minecraft@gmail.com>
2022-06-24 19:50:46 +0200

    MVC avancé un peu

--- controller/offers_detail.php
+++ controller/offers_detail.php
@@ -6,23 +6,33 @@ include "./php/db.php"; //Used to get global pdo
 class OfferDetailsController{
 
 
-function getOfferdetail(){
-    try{
-        $offer_details = new OfferDetail();
-        return $offer_details->getOfferdetail(8);
-    }catch(Exception $e){
-        return "Error : " . $e;
+    function getOfferdetail($id_offer){
+        try{
+            $offer_details = new OfferDetail();
+            return $offer_details->getOfferdetail($id_offer);
+        }catch(Exception $e){
+            return "Error : " . $e;
+        }
     }
-}
 
-function hasAlreadySentCandidature(){
-    try{
-    $offer_details = new OfferDetail();
-        return $offer_details->hasAlreadySentCandidature;
-    }catch(Exception $e){
-        return "Error : " . $e;
+    function hasAlreadySentCandidature(){
+        try{
+        $offer_details = new OfferDetail();
+            return $offer_details->hasAlreadySentCandidature();
+        }catch(Exception $e){
+            return "Error : " . $e;
+        }
     }
-}
+
+    function getCompanyFromOffer($id_offer){
+        try{
+        $offer_details = new OfferDetail();
+            return $offer_details->getCompanyFromOffer($id_offer);
+        }catch(Exception $e){
+            return "Error : " . $e;
+        }
+    }
+    
 
 }
 ?>
